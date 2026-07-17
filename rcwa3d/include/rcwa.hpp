c6f081e58e3b84389f70d49864ab09ba0ef326ba
LukhaGian <lukhagian@gmail.com>
2026-07-17 15:11:44 +0200

    Added first structure for CPU-based 3D RCWA solver.

---
+++ rcwa3d/include/rcwa.hpp
@@ -0,0 +1,202 @@
+#pragma once
+
+#include <vector>
+#include <complex>
+#include <Eigen/Dense>
+#define _USE_MATH_DEFINES
+#include <cmath>
+//#include <iostream>
+
+using namespace std::complex_literals; // Enables the '1i' literal
+using Real = double;
+using Complex = std::complex<Real>;
+using Matrix = Eigen::Matrix<Complex, Eigen::Dynamic, Eigen::Dynamic>; // check properties, it is a complex matrix with dynamic size (BY DEFAULT COLUMN MAJOR ORDER)
+using Vector = Eigen::Matrix<Complex, Eigen::Dynamic, 1>;
+
+using MatrixXcd = Eigen::MatrixXcd; 
+using VectorXcd = Eigen::VectorXcd;
+
+
+
+// struct that defines the Device properties
+struct Device 
+{
+    // grid resolution per layer
+    int Nx{}; // spatial grid points along X axis (columns)
+    int Ny{}; // spatial grid points along Y axis (rows)
+    int num_layers{};  // number of layers M
+    Real Lx{};        // unit cell dimensions
+    Real Ly{};
+    // 3D arrays: er[iy][ix][layer] stored as flat vector er[layer * Nx * Ny + iy * Nx + ix] (row-major order) --> be careful and TBD
+
+    // spatial arrays
+    std::vector<Real> er{};  // permittivity Ny × Nx x num_layers
+    std::vector<Real> ur{};  // permeability Ny × Nx x num_layers
+    std::vector<Real> t{};   // thickness per layer, size num_layers
+
+    // convolution space arrays: TBD if 3d Tensor solution is better or std::vector<Matrix> suffices (iterating over layers)
+    std::vector<Matrix> erc{}; // convolution of perimittivity er num_layers x Ny_harmonics × Nx_harmonics
+    std::vector<Matrix> urc{}; // convolution of permeability ur num_layers x Ny_harmonics × Nx_harmonics
+
+    Device() : Nx(1), Ny(1), num_layers(1), Lx(1.0), Ly(1.0) // Default constructor, add the remaining default entries for the field DONT USE IT FOR NOW
+    {
+    }
+
+    Device(int Nx_, int Ny_, int num_layers_, Real Lx_, Real Ly_, const std::vector<Real>& er_, 
+        const std::vector<Real>& ur_, const std::vector<Real>& t_, int Nx_harmonics_, int Ny_harmonics_) 
+        : Nx(Nx_), Ny(Ny_), num_layers(num_layers_), Lx(Lx_), Ly(Ly_), er(er_), ur(ur_), t(t_),
+        erc(num_layers_, Matrix::Zero(Ny_harmonics_, Nx_harmonics_)), urc(num_layers_, Matrix::Zero(Ny_harmonics_, Nx_harmonics_))
+    {
+    }
+};
+
+// struct that defines the Source properties
+struct Source
+{
+    Real lambda0{}; // wavelength in vacuum
+    Real k0{};    // wave number in vacuum = 2pi/lambda0
+    Real theta{}; // polar angle of incidence
+    Real phi{}; // azimuthal angle of incidence
+    //
+    Real pte{};       // TE polarisation amplitude
+    Real ptm{};       // TM polarisation amplitude
+
+    Source(Real lambda0_, Real theta_, Real phi_, Real pte_, Real ptm_) 
+    : lambda0(lambda0_), k0(2 * M_PI / lambda0_), theta(theta_), phi(phi_), pte(pte_), ptm(ptm_)
+    {
+    }
+};
+
+// struct that defines the RCWA simulation parameters
+struct RCWAParams
+{
+    int Nx_harmonics{}; // number of harmonics along X axis
+    int Ny_harmonics{}; // number of harmonics along Y axis
+    //
+    Real er_ref{};    // permittivity of reflection region (superstrate)
+    Real ur_ref{};    // permeability of reflection region
+    Real er_trn{};    // permittivity of transmission region (substrate)
+    Real ur_trn{};    // permeability of transmission region
+
+    RCWAParams(int Nx_harmonics_, int Ny_harmonics_, Real er_ref_, Real ur_ref_, Real er_trn_, Real ur_trn_)
+    : Nx_harmonics(Nx_harmonics_), Ny_harmonics(Ny_harmonics_), er_ref(er_ref_), ur_ref(ur_ref_), er_trn(er_trn_), ur_trn(ur_trn_)
+    {
+    }
+};
+
+
+// struct that defines the RCWA results, i.e. reflection and transmission
+struct Results
+{
+    std::vector<Real> R{}; // reflection coefficients for each diffraction order
+    std::vector<Real> T{}; // transmission coefficients for each diffraction order
+    Real R_tot{}; // total reflection
+    Real T_tot{}; // total transmission
+    // Note: for energy conservation, R_tot + T_tot = 1
+};
+
+
+// struct that contains a Scattering Matrix
+struct ScatteringMatrix
+{
+    // S-matrix blocks for each layer
+    Matrix S11{}; // reflection from the left
+    Matrix S12{}; // transmission from left to right
+    Matrix S21{}; // transmission from right to left
+    Matrix S22{}; // reflection from the right
+    
+    ScatteringMatrix(Matrix S11_, Matrix S12_, Matrix S21_, Matrix S22_) : S11(S11_), S12(S12_), S21(S21_), S22(S22_)
+    {
+    }
+    /*
+    ScatteringMatrix() = default;
+
+    ScatteringMatrix(const Matrix& a, const Matrix& b,
+                     const Matrix& c, const Matrix& d)
+        : S11(a), S12(b), S21(c), S22(d) {
+            std::cout << "constructor\n";
+        }
+
+    ScatteringMatrix(const ScatteringMatrix& other) : S11(other.S11), S12(other.S12), S21(other.S21), S22(other.S22) {
+        std::cout << "copy\n";
+    }
+
+    ScatteringMatrix(ScatteringMatrix&& other) noexcept
+        : S11(std::move(other.S11)), S12(std::move(other.S12)),
+          S21(std::move(other.S21)), S22(std::move(other.S22)) {
+        std::cout << "move\n";
+    }
+    */
+};
+
+
+// Computation flow
+
+// 1. Initialization
+
+// 2. Build device layers on grid (initialize the Device)
+
+// 3. Compute the Convolution matrices for each layer and each field (er, ur)
+Matrix ConvMat(const std::vector<Real>& field, int layer, int Nx, int Ny, int Nx_harmonics, int Ny_harmonics); // add then in place the convolution matrices for each layer and each field (er, ur) into the Device struct
+
+// 4. Compute Wave Vector Expansion, ie update k_inc, Kx, Ky, Kz_ref, Kz_trn
+void ComputeWaveVectors(const Device& device, const Source& source, const RCWAParams& params, std::vector<Real>& k_inc, Vector& Kx, Vector& Ky, Vector& Kz_ref, Vector& Kz_trn);
+
+// 5. Compute eigenmodes of Gap medium, ie compute W0 and V0 for the gap medium
+void GapMedium(const Vector& Kx, const Vector& Ky, Matrix& W0, Matrix& V0);
+
+// 6. Initialize the Device Scattering Matrix, ie return the initial Scattering Matrix S_device
+ScatteringMatrix SMatrixInit(int Nx_harmonics, int Ny_harmonics);
+
+// 7. Iteration per layer (build eigen value problem per layer), compute the S-matrix for each layer, and combine them using Redheffer product wrt S_device
+ScatteringMatrix SMatrixLayer(int layer, const Device& device, const Source& source, const RCWAParams& params, const Vector& Kx, const Vector& Ky, const Matrix& W0, const Matrix& V0);
+// remember to .resize(0,0) to free the memory when the S-matrix is no longer needed, e.g. after Redheffer product, OR use blocks in main for sequential
+
+// Update Device matrix
+
+// 8. Compute Reflection Side Connection SMatrix, ie S_ref
+ScatteringMatrix SMatrixReflection(const RCWAParams& params, const Vector& Kx, const Vector& Ky, Vector& Kz_ref, const Matrix& W0, const Matrix& V0);
+
+// 9. Compute Transmission Side Connection SMatrix, ie S_trn
+ScatteringMatrix SMatrixTransmission(const RCWAParams& params, const Vector& Kx, const Vector& Ky, Vector& Kz_trn, const Matrix& W0, const Matrix& V0);
+
+// 10. Compute Global SMatrix, S_global = S_ref x S_device x S_trn
+// call RedhefferProduct() to combine the S-matrices of ref + device + trn into global
+
+// 11. Compute Source parameters (source field)
+// compute esrc and csrc 
+void ComputeSourceField(const Source& source, const RCWAParams& params, const std::vector<Complex>& k_inc, std::vector<Complex>& esrc, std::vector<Complex>& csrc);
+
+// 12. Compute Reflected and Transmitted Fields
+// reflected field r
+std::vector<Complex> ComputeReflectedField(const ScatteringMatrix& S_global, const std::vector<Complex>& csrc, const Matrix& Kx, const Matrix& Ky, const Matrix& Kz_ref); // W_ref is considered = I
+// transmitted field t
+std::vector<Complex> ComputeTransmittedField(const ScatteringMatrix& S_global, const std::vector<Complex>& csrc, const Matrix& Kx, const Matrix& Ky, const Matrix& Kz_trn); // W_trn is considered = I
+
+// 13. Compute Diffraction Efficiencies
+Results ComputeDiffractionEfficiencies(const RCWAParams& params, std::vector<Complex> r, std::vector<Complex> t, const std::vector<Complex>& k_inc, const Matrix& Kz_ref, const Matrix& Kz_trn);
+
+// 14. Output results
+void OutputResults(const Results& results, const RCWAParams& params);
+
+// MOVE TO ADDITIONAL?
+// Redheffer Product A x B
+ScatteringMatrix RedhefferProduct(const ScatteringMatrix& A, const ScatteringMatrix& B); 
+// remember to use std::move() to avoid copying the resulting S-matrix in place AND .resize(0,0) to free the memory when the S-matrix is no longer needed, e.g. after Redheffer product
+
+// MeshGrid function, template
+template <typename Scalar>
+void MeshGrid(const Eigen::Matrix<Scalar, Eigen::Dynamic, 1>& x, const Eigen::Matrix<Scalar, Eigen::Dynamic, 1>& y,
+              Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>& X, Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>& Y)
+{
+    X = x.transpose().replicate(y.size(), 1);
+    Y = y.replicate(1, x.size());
+}
+
+struct EigenvalSolverResults
+{
+    Vector eigenvalues{};
+    Matrix eigenvectors{};
+};
+
+EigenvalSolverResults extraction(Eigen::ComplexEigenSolver<Matrix>&& solver);
---
+++ rcwa3d/src/rcwa.cpp
@@ -0,0 +1,306 @@
+// #include "../include/rcwa.hpp"
+#include "rcwa.hpp"
+#include <unsupported/Eigen/FFT>
+#include <iostream>
+
+Matrix ConvMat(const std::vector<Real>& field, int layer, int Nx, int Ny, int Nx_harmonics, int Ny_harmonics)
+{                 
+    /*
+    Function that returns the 2D convolution matrix for a given field for a specific layer.
+    The input field is assumed to be a 2D array of size Ny x Nx, and the output convolution matrix will be of size
+    PQ x PQ, where P = (2 * Nx_harmonics + 1) and Q = (2 * Ny_harmonics + 1)
+    -> the convolution matrix is of size (2 * Nx_harmonics + 1) * (2 * Ny_harmonics + 1) x (2 * Nx_harmonics + 1) * (2 * Ny_harmonics + 1)
+    */
+
+    // Map the input field to a row-major matrix of size Ny x Nx
+    using RowMajorMatrixXdMap = Eigen::Map<const Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>;
+    //                                  raw pointer of data + move it to the specific layer, then map it to a row-major matrix of size Ny x Nx
+    RowMajorMatrixXdMap realRowMajorMap(field.data() + layer * Nx * Ny, Ny, Nx);
+    // cast the real row-major matrix to a complex column-major matrix of size Ny x Nx (default Eigen matrix is column-major)
+    Matrix F = realRowMajorMap.cast<Complex>();
+
+    // FFT computation
+    Eigen::FFT<Real> fft;
+    // By row
+    for (int iy = 0; iy < Ny; ++iy)
+    {
+        Vector row_fft;
+        fft.fwd(row_fft, F.row(iy));
+        F.row(iy) = row_fft;
+    }
+    // By column
+    for (int ix = 0; ix < Nx; ++ix)
+    {
+        Vector col_fft;
+        fft.fwd(col_fft, F.col(ix));
+        F.col(ix) = col_fft;
+    }
+
+    // Normalization of the FFT
+    F /= static_cast<Real>(Nx * Ny);
+
+    // FFT shift to center the zero frequency component
+    Matrix F_shift(Ny, Nx);
+    int half_Nx = Nx /2;
+    int half_Ny = Ny /2;
+    // F = [Q1 Q2]
+    //     [Q3 Q4]
+    // flip blocks -> Q1 to Q4, Q2 to Q3, Q3 to Q2, Q4 to Q1
+    F_shift.block(half_Ny, half_Nx, Ny-half_Ny, Nx-half_Nx) = F.block(0, 0, Ny-half_Ny, Nx-half_Nx); // Q1 to Q4
+    F_shift.block(0,0, half_Ny, half_Nx) = F.block(Ny-half_Ny, Nx-half_Nx, half_Ny, half_Nx); // Q4 to Q1
+    F_shift.block(half_Ny, 0, Ny-half_Ny, half_Nx) = F.block(0, Nx-half_Nx, Ny-half_Ny, half_Nx); // Q2 to Q3
+    F_shift.block(0, half_Nx, half_Ny, Nx-half_Nx) = F.block(Ny-half_Ny, 0, half_Ny, Nx-half_Nx); // Q3 to Q2
+
+    // Toeplitz matrix assembly of size PQ x PQ
+    int P = 2 * Nx_harmonics + 1;
+    int Q = 2 * Ny_harmonics + 1;
+    int PQ = P * Q;
+    Matrix C = Matrix::Zero(PQ, PQ);
+    
+    for (int m = 0; m < PQ; ++m)
+    {
+        int py_m = m / P - Ny_harmonics; // y index of m
+        int px_m = m % P - Nx_harmonics; // x index of m
+
+        for (int n = 0; n < PQ; ++n)
+        {
+            int py_n = n / P - Ny_harmonics; // y index of n
+            int px_n = n % P - Nx_harmonics; // x index of n
+
+            int kx_idx = (px_m - px_n) + half_Nx; // index in the FFT shifted matrix along x
+            int ky_idx = (py_m - py_n) + half_Ny; // index in the FFT shifted matrix along y
+            if (kx_idx >= 0 && kx_idx < Nx && ky_idx >= 0 && ky_idx < Ny)
+            {
+                C(m, n) = F_shift(ky_idx, kx_idx);
+            }
+        }
+    }
+
+    return C;
+
+}
+
+
+void ComputeWaveVectors(const Device& device, const Source& source, const RCWAParams& params, std::vector<Real>& k_inc, Vector& Kx, Vector& Ky, Vector& Kz_ref, Vector& Kz_trn)
+{
+    /*
+    Function that computes the Wave Vector Expansion. NOTE: WE ARE WORKING WITH RealS, IN THE END CAST TO COMPLEX RealS
+    */
+    // incidence vector
+    Real n_inc = std::sqrt(params.er_ref * params.ur_ref); // refractive index, FOR NOW ONLY DIELETRIC MATERIALS (n_inc is NOT COMPLEX)
+    Real sin_theta = std::sin(source.theta);
+    Real cos_theta = std::cos(source.theta);
+    Real sin_phi = std::sin(source.phi);
+    Real cos_phi = std::cos(source.phi);
+    k_inc[0] = n_inc * sin_theta * cos_phi;
+    k_inc[1] = n_inc * sin_theta * sin_phi;
+    k_inc[2] = n_inc * cos_theta;
+
+    // Wave vector components
+    Eigen::VectorXd k_x_tilde = Eigen::VectorXd::LinSpaced(2*params.Nx_harmonics + 1, -params.Nx_harmonics, params.Nx_harmonics);
+    //std::cout << k_x_tilde << '\n';
+    Eigen::VectorXd k_y_tilde = Eigen::VectorXd::LinSpaced(2*params.Ny_harmonics + 1, -params.Ny_harmonics, params.Ny_harmonics);
+    //std::cout << '\n';
+    //std::cout << k_y_tilde << '\n';
+    k_x_tilde *= -(source.lambda0 / device.Lx);
+    //k_x_tilde *= -(2 * M_PI / (source.k0 * device.Lx));
+    k_x_tilde.array() += k_inc[0];
+    k_y_tilde *= -(source.lambda0 / device.Ly);
+    //k_y_tilde *= -(2 * M_PI / (source.k0 * device.Ly));
+    k_y_tilde.array() += k_inc[1];
+    Eigen::MatrixXd Kx_tilde = Eigen::MatrixXd::Zero(k_y_tilde.size(), k_x_tilde.size());
+    Eigen::MatrixXd Ky_tilde = Eigen::MatrixXd::Zero(k_y_tilde.size(), k_x_tilde.size());
+    MeshGrid(k_x_tilde, k_y_tilde, Kx_tilde, Ky_tilde);
+    //std::cout << Kx_tilde << '\n';
+    //std::cout << '\n';
+    //std::cout << Ky_tilde << '\n';
+    // Longitudinal vector components in the reflection and transmission region
+    // NOTE: REWRITE IN CASE OF NON DIELETRIC MATERIALS
+    Matrix k_z_ref = -(params.ur_ref*params.er_ref - Kx_tilde.array().square() - Ky_tilde.array().square()).cast<Complex>().sqrt();
+    Matrix k_z_trn = (params.ur_trn*params.er_trn - Kx_tilde.array().square() - Ky_tilde.array().square()).cast<Complex>().sqrt();
+
+    Kx = Kx_tilde.reshaped();
+    Ky = Ky_tilde.reshaped();
+    Kz_ref = k_z_ref.reshaped(); // a little bit of inconsistency
+    Kz_trn = k_z_trn.reshaped();
+    //std::cout << k_z_ref << '\n';
+    //std::cout << k_z_ref.rows() << " " << k_z_ref.cols() << '\n';
+    //std::cout << Kz_ref << '\n';
+    //std::cout << '\n';
+
+    // NOTE: REMEMBER TO USE THE MATRICES/VECTORS Kx, Ky, Kz_ref AND Kz_trn WITH THE CALL .asDiagonal()
+}
+
+
+void GapMedium(const Vector& Kx, const Vector& Ky, Matrix& W0, Matrix& V0)
+{
+    /*
+    Compute Eigenmodes of the Gap medium. To be revised!!! ==================================
+    */
+    Vector Kz = (1.0 - Kx.array().square() - Ky.array().square()).sqrt().conjugate();
+    int block_size = Kz.size();
+
+    Vector Kx_Ky = Kx.array() * Ky.array();
+    //Vector main_diag(Kx_Ky.size()*2);
+    //main_diag << Kx_Ky, -Kx_Ky;
+    Matrix top_left_Mat = Kx_Ky.asDiagonal();
+    std::cout << top_left_Mat << '\n';
+    std::cout << '\n';
+    std::cout << top_left_Mat.rows() << " " << top_left_Mat.cols() << '\n';
+    Vector top_right = 1.0 - Kx.array().square();
+    Matrix top_right_Mat = top_right.asDiagonal();
+    Vector bottom_left = Ky.array().square() - 1.0;
+    Matrix bottom_left_Mat = bottom_left.asDiagonal();
+    Matrix bottom_right_Mat = - top_left_Mat;
+    Matrix Q(2*block_size, 2*block_size);
+    //Q << Kx_Ky.asDiagonal(), top_right.asDiagonal(), bottom_left.asDiagonal(), (-Kx_Ky).asDiagonal();
+    Q.block(0, 0, block_size, block_size) = Kx_Ky.asDiagonal();
+    Q.block(0, block_size, block_size, block_size) = top_right.asDiagonal();
+    Q.block(block_size, 0, block_size, block_size) = bottom_left.asDiagonal();
+    Q.block(block_size, block_size, block_size, block_size) = (-bottom_left).asDiagonal();
+    //Q.block(0, block_size, block_size+1, )
+    //Q << top_left_Mat, top_right_Mat, bottom_left_Mat, bottom_right_Mat;
+    //std::cout << Q <<'\n';
+    //std::cout << 
+    W0 = Matrix::Identity(2*block_size, 2*block_size);
+
+    Vector lambda(2*block_size);
+    lambda << 1i*Kz, 1i*Kz;
+    std::cout << lambda << '\n';
+    // Possible instability for lambda
+    Matrix Test = Q * lambda.array().inverse().matrix().asDiagonal(); // check if .matrix is really needed
+   
+    //V0 = Q.array().rowwise() / lambda.array();
+    // check if it's the best way to do this
+    for (int j = 0; j < Q.cols(); ++j)
+    {
+        V0.col(j) = Q.col(j) / lambda(j); // are we potentially dividing for 0? Yeah....
+    }
+    if (V0.isApprox(Test))
+        std::cout << "Similar" << '\n';
+    //std::cout << V0 << '\n';
+
+}
+
+
+ScatteringMatrix SMatrixInit(int Nx_harmonics, int Ny_harmonics)
+{
+    /*
+    Function that initialize the device Scattering matrix. The various components are interpreted as:
+    S11 = 0 --> reflection at the first interface (init as zeros)
+    S12 = I --> transmission in the forward direction
+    S21 = I --> transmission in the backward direction
+    S22 = 0 --> reflection at the last interface
+    */
+    int P = 2 * Nx_harmonics + 1;
+    int Q = 2 * Ny_harmonics + 1;
+    int PQ = P * Q;
+    return ScatteringMatrix(Matrix::Zero(2*PQ, 2*PQ), Matrix::Identity(2*PQ, 2*PQ), Matrix::Identity(2*PQ, 2*PQ), Matrix::Zero(2*PQ, 2*PQ));
+}
+
+
+
+ScatteringMatrix SMatrixLayer(int layer, const Device& device, const Source& source, const RCWAParams& params, const Vector& Kx, const Vector& Ky, const Matrix& W0, const Matrix& V0)
+{
+    /*
+    Function that computes the layer scattering matrix for the given ith layer. TO BE TESTED
+    */
+    int P = 2 * params.Nx_harmonics + 1;
+    int Q = 2 * params.Ny_harmonics + 1;
+    int PQ = P * Q;
+    // Build eigenvalue problem
+    // P_i
+    Matrix P_i = Matrix::Zero(2*PQ, 2*PQ);
+    // solving column-by-column in a loop THINK ABOUT IT, IT SHOULD BE WAY MORE EFFICIENT!!! ======================== we have a diagonal matrix on the right hand side after all
+    //Matrix Kx_Mat = Kx.asDiagonal();
+    //                             TO BE SUBSTITUTED WITH NO BOUND CHECK [], faster
+    //Matrix erc_inv_Kx = device.erc.at(layer).lu().solve(Kx_Mat);
+    //                                                  view of Kx creates a temporary full dense matrix ==> A bit of a waste!!!!
+    Matrix erc_inv_Kx = device.erc.at(layer).lu().solve(Kx.asDiagonal().toDenseMatrix());
+    Matrix erc_inv_Ky = device.erc.at(layer).lu().solve(Ky.asDiagonal().toDenseMatrix());
+    P_i.block(0, 0, PQ, PQ) = Kx.asDiagonal() * erc_inv_Ky; // top left
+    P_i.block(0, PQ, PQ, PQ) = device.urc.at(layer) - Kx.asDiagonal() * erc_inv_Kx; // top right
+    P_i.block(PQ, 0, PQ, PQ) = Ky.asDiagonal() * erc_inv_Ky - device.urc.at(layer); // bottom left
+    P_i.block(PQ, PQ, PQ, PQ) = (-Ky).asDiagonal() * erc_inv_Kx; // bottom right
+    // Q_i
+    Matrix Q_i = Matrix::Zero(2*PQ, 2*PQ);
+    Matrix urc_inv_Kx = device.urc.at(layer).lu().solve(Kx.asDiagonal().toDenseMatrix());
+    Matrix urc_inv_Ky = device.urc.at(layer).lu().solve(Ky.asDiagonal().toDenseMatrix());
+    Q_i.block(0, 0, PQ, PQ) = Kx.asDiagonal() * urc_inv_Ky;
+    Q_i.block(0, PQ, PQ, PQ) = device.erc.at(layer) - Kx.asDiagonal() * urc_inv_Kx;
+    Q_i.block(PQ, 0, PQ, PQ) = Ky.asDiagonal() * urc_inv_Ky - device.erc.at(layer);
+    Q_i.block(PQ, PQ, PQ, PQ) = (-Ky).asDiagonal() * urc_inv_Kx;
+
+    Matrix Omega2 = P_i * Q_i;
+
+    // solve eigenvalue problem Omega2 * x = lambda * x
+    // In general EigenSolver<Matrix> solver(A)
+    // solver.eigenvalues()
+    // solver.eigenvectors()
+    // if A = A^T --> use SelfAdjointEigenSolver<Matrix> solver(A)
+    // expA = A.matrixExponential();
+    Eigen::ComplexEigenSolver<Matrix> solver(Omega2);
+
+    if (solver.info() != Eigen::Success) abort();
+
+    std::cout << solver.eigenvectors() << '\n';
+    std::cout << solver.eigenvalues() << '\n';
+
+    auto [lambda2, W_i] = extraction(std::move(solver));
+
+    std::cout << lambda2 << '\n';
+    std::cout << W_i << '\n';
+
+    // Check a more elegant way of computing it =======================
+    Matrix V_i = Q * W_i * lambda2.array().sqrt().inverse().matrix().asDiagonal();
+    Matrix W_i_inv_W0 = W_i.lu().solve(W0);
+    Matrix V_i_inv_V0 = V_i.lu().solve(V0);
+    Matrix A_i0 = W_i_inv_W0  + V_i_inv_V0;
+    Matrix B_i0 = W_i_inv_W0 - V_i_inv_V0;
+    Matrix X_i = (-source.k0 * device.t[layer] * lambda2.array().sqrt()).exp().matrix().asDiagonal(); // Matrix Exponential
+
+    // Assemble S matrix
+    // Lazy impl, enable NRVO next
+    Matrix partial_prod = A_i0 - X_i * B_i0 * A_i0.lu().solve(X_i) * B_i0;
+    Matrix S11 = partial_prod.lu().solve(X_i * B_i0 * A_i0.lu().solve(X_i) * A_i0 - B_i0);
+    Matrix S12 = partial_prod.lu().solve(X_i) * (A_i0 - B_i0 * A_i0.lu().solve(B_i0));
+
+    return ScatteringMatrix(S11, S12, S12, S11);
+}
+
+
+
+ScatteringMatrix RedhefferProduct(const ScatteringMatrix& A, const ScatteringMatrix& B)
+{
+    /*
+    Function that computes the Redheffer star product of two scattering matrices A and B.
+    The Redheffer star product is defined as follows:
+    S = A x B = [S11, S12; S21, S22] where
+    S11 = B11 * (I - A12 * B21)^(-1) * A11
+    S12 = B12 + B11 * (I - A12 * B21)^(-1) * A12 * B22
+    S21 = A21 + A22 * (I - B21 * A12)^(-1) * B21 * A11
+    S22 = A22 * (I - B21 * A12)^(-1) * B22,
+    where Xnm = X.Snm, and I is the identity matrix of appropriate size.
+    */
+
+    // To be merged for efficiency
+    Matrix I_A12_B21 = Matrix::Identity(A.S12.rows(), B.S21.cols()) - A.S12 * B.S21;
+    Matrix I_B21_A12 = Matrix::Identity(B.S21.rows(), A.S12.cols()) - B.S21 * A.S12;
+
+    auto lu_I_A12_B21 = I_A12_B21.transpose().lu();
+    auto lu_I_B21_A12 = I_B21_A12.transpose().lu();
+
+    Matrix shared_first_row_block = lu_I_A12_B21.solve(B.S11.transpose()).transpose();
+    Matrix shared_second_row_block = lu_I_B21_A12.solve(A.S22.transpose()).transpose();
+    
+
+    return ScatteringMatrix(shared_first_row_block * A.S11, B.S12 + shared_first_row_block * A.S12 * B.S22,
+                            A.S21 + shared_second_row_block * B.S21 * A.S11, shared_second_row_block * B.S22);
+
+}
+
+EigenvalSolverResults extraction(Eigen::ComplexEigenSolver<Matrix>&& solver)
+{
+        // Return values are constructed directly in the caller's memory frame
+    return EigenvalSolverResults{solver.eigenvalues(), solver.eigenvectors()};
+}
